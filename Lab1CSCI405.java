bda91186b8212e3660d0017a99bd4b0d589ea4fb
mattmcara04 <mam026@marietta.edu>
2026-09-02 18:11:30 -0400

    Add Java Program

---
+++ Lab1CSCI405.java
@@ -0,0 +1,8 @@
+Java
+public class Lab1CSCI405{
+    public static void main(String[] args){
+        system.out.println("Name: Matthew McAra");
+        system.out.println("Major: Computer Science");
+        system.out.println("First Job: Help Desk Technician");
+    }
+}
